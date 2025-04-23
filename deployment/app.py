855f7dcbe6819f03c9923a93a6e5bea0fe91f5e3
Hennoshin <m.irfan.hilmi@gmail.com>
2025-04-23 23:48:36 +0700

    Add temperature control callback

--- deployment/app.py
+++ deployment/app.py
@@ -1,3 +1,4 @@
+from matplotlib.pyplot import flag
 import streamlit as st
 
 import json
@@ -20,8 +21,58 @@ class MainApp:
         with open(self.features_path, "r") as file_2:
             self.features_values = json.load(file_2)
 
+    def temp_callback(self, slider_temp):
+        # Check condition
+        min = st.session_state.min_temp
+        max = st.session_state.max_temp
+
+        flag_min_max = (max < min)
+        flag_9am = (st.session_state.temp_9am < min) or (st.session_state.temp_9am > max)
+        flag_3pm = (st.session_state.temp_3pm < min) or (st.session_state.temp_3pm > max)
+        if not (flag_min_max or flag_9am or flag_3pm):
+            return
+        
+        if (slider_temp == "min_temp"):
+            if (flag_min_max):
+                st.session_state.max_temp = min
+            if (flag_9am):
+                st.session_state.temp_9am = min
+            if (flag_3pm):
+                st.session_state.temp_3pm = min
+
+        elif (slider_temp == "max_temp"):
+            if (flag_min_max):
+                st.session_state.min_temp = max
+            if (flag_9am):
+                st.session_state.temp_9am = max
+            if (flag_3pm):
+                st.session_state.temp_3pm = max
+
+        elif (slider_temp == "temp_9am"):
+            if st.session_state.temp_9am < min:
+                st.session_state.min_temp = st.session_state.temp_9am
+            else:
+                st.session_state.max_temp = st.session_state.temp_9am
+
+        elif (slider_temp == "temp_3pm"):
+            if st.session_state.temp_3pm < min:
+                st.session_state.min_temp = st.session_state.temp_3pm
+            else:
+                st.session_state.max_temp = st.session_state.temp_3pm
+
+
     def run(self):
         st.title("Rain Prediction App")
+
+        # Session states
+        if "min_temp" not in st.session_state:
+            st.session_state.min_temp = self.features_values["MinTemp"] - 2
+        if "max_temp" not in st.session_state:
+            st.session_state.max_temp = self.features_values["MinTemp"]
+        if "temp_9am" not in st.session_state:
+            st.session_state.min_temp = self.features_values["MinTemp"] - 1
+        if "temp_3pm" not in st.session_state:
+            st.session_state.max_temp = self.features_values["MinTemp"] - 1
 
         locations = self.features_values["Location"]
         wind_dir = self.features_values["WindDir"]
@@ -29,10 +80,10 @@ class MainApp:
 
         # Input fields
         location = st.selectbox("Weather Station", locations)
-        min_temp = st.slider("Minimum Temperature (°C)", min_value=self.features_values["MinTemp"] - 2, max_value=self.features_values["MaxTemp"] + 2)
-        max_temp = st.slider("Maximum Temperature (°C)", min_value=self.features_values["MinTemp"] - 2, max_value=self.features_values["MaxTemp"] + 2)
-        temp_9am = st.slider("Temperature 9am (°C)", min_value=self.features_values["MinTemp"] - 2, max_value=self.features_values["MaxTemp"] + 2)
-        temp_3pm = st.slider("Temperature 3pm (°C)", min_value=self.features_values["MinTemp"] - 2, max_value=self.features_values["MaxTemp"] + 2)
+        min_temp = st.slider("Minimum Temperature (°C)", min_value=self.features_values["MinTemp"] - 2, max_value=self.features_values["MaxTemp"] + 2, value=st.session_state.min_temp, key="min_temp", on_change=(lambda: self.temp_callback("min_temp")))
+        max_temp = st.slider("Maximum Temperature (°C)", min_value=self.features_values["MinTemp"] - 2, max_value=self.features_values["MaxTemp"] + 2, value=st.session_state.max_temp, key="max_temp", on_change=(lambda: self.temp_callback("max_temp")))
+        temp_9am = st.slider("Temperature 9am (°C)", min_value=self.features_values["MinTemp"] - 2, max_value=self.features_values["MaxTemp"] + 2, value=st.session_state.temp_9am, key="temp_9am", on_change=(lambda: self.temp_callback("temp_9am")))
+        temp_3pm = st.slider("Temperature 3pm (°C)", min_value=self.features_values["MinTemp"] - 2, max_value=self.features_values["MaxTemp"] + 2, value=st.session_state.temp_3pm, key="temp_3pm", on_change=(lambda: self.temp_callback("temp_3pm")))
         humidity_9am = st.slider("Humidity 9am (%)", min_value=self.features_values["HumidityMin"], max_value=self.features_values["HumidityMax"])
         humidity_3pm = st.slider("Humidity 3pm (%)", min_value=self.features_values["HumidityMin"], max_value=self.features_values["HumidityMax"])
         pressure_9am = st.slider("Pressure 9am (hpa)", min_value=self.features_values["PressureMin"], max_value=self.features_values["PressureMax"])
@@ -76,7 +127,6 @@ class MainApp:
             "Temp3pm": temp_3pm,
             "RainToday": "Yes" if rainfall >= 1.0 else "No"
         }
-
 
         if st.button("Predict"):
             prediction = self.model.predict(pd.DataFrame(input))
